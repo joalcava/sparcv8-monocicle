71a8ddedfc82c54975aca1f7fa792c9d8e9ec3ef
Fernando Giraldo Duque <ferchogir@gmail.com>
2016-10-06 16:53:38 -0500

    Update control_unit.vhd

--- control_unit.vhd
+++ control_unit.vhd
@@ -1,33 +1,41 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    17:07:38 10/04/2016 
--- Design Name: 
--- Module Name:    asdf - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity control_unit is
-end control_unit;
+entity UC is
+    Port ( op3 : in  STD_LOGIC_VECTOR(5 downto 0);
+           op : in  STD_LOGIC_VECTOR(1 downto 0);
+           awop : out  STD_LOGIC_VECTOR(5 downto 0)
+			  );
+end UC;
 
-architecture Behavioral of control_unit is
-
-begin
+architecture ArqUC of UC is
 
 
-end Behavioral;
+
+begin 
+	process(op3)
+		begin
+		case (op3) is
+					when "000000"=>
+						awop<="000000";
+					when "000100"=>
+						awop<="000001";
+					when "000010"=>
+						awop<="000010";
+					when "000001"=>
+						awop<="000011";
+					when "000011"=>
+						awop<="000100";
+					when "000110"=>
+						awop<="000101";
+					when "000101"=>
+						awop<="000110";
+					when "000111"=>
+						awop<="000111";
+					when others=>
+						awop<="000000";
+		end case;
+	end process;	
+
+end ArqUC;
 
